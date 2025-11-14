3dcdea869860d7405f5a7417be6f128791d38324
MaryanBog <96717999+MaryanBog@users.noreply.github.com>
2025-11-15 00:00:34 +0300

    Update e_operator.h

--- fcs-sdk/include/fcs/operators/e_operator.h
+++ fcs-sdk/include/fcs/operators/e_operator.h
@@ -1,1 +1,12 @@
+#ifndef FCS_SDK_E_OPERATOR_H
+#define FCS_SDK_E_OPERATOR_H
+
+namespace fcs {
+
+    // Линейный контрактор: E(x) = alpha * x  (0 < alpha < 1)
+    double default_E(double x);
+
+}
+
+#endif
 
