f8ad7712663f6a90380208305fc019ebfbdb1b08
MaryanBog <96717999+MaryanBog@users.noreply.github.com>
2025-11-15 00:02:02 +0300

    Update g_operator.cpp

--- fcs-sdk/src/g_operator.cpp
+++ fcs-sdk/src/g_operator.cpp
@@ -1,1 +1,12 @@
+#include "g_operator.h"
+
+namespace fcs {
+
+    static const double kG = 1.0;  // коэффициент управления
+
+    double default_G(double delta) {
+        return kG * delta;
+    }
+
+}
 
