25c01e81e541df0973ed790966c4fcd653a14336
MaryanBog <96717999+MaryanBog@users.noreply.github.com>
2025-11-15 11:55:53 +0300

    Update finv_operator.cpp

--- fcs-sdk/src/finv_operator.cpp
+++ fcs-sdk/src/finv_operator.cpp
@@ -1,7 +1,8 @@
-#include "finv_operator.h"
+#include "fcs/operators/finv_operator.h"
 
 namespace fcs {
 
+    // Обратный к F оператор (если F = kF * x)
     static const double kF = 1.0;
 
     double default_FInv(double x) {
@@ -9,4 +10,3 @@ namespace fcs {
     }
 
 }
-
