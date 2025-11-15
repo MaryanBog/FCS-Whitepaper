acdf083124b8d68026b08883acaf09b3c13343be
MaryanBog <96717999+MaryanBog@users.noreply.github.com>
2025-11-15 11:54:50 +0300

    Update f_operator.cpp

--- fcs-sdk/src/f_operator.cpp
+++ fcs-sdk/src/f_operator.cpp
@@ -1,12 +1,12 @@
-#include "f_operator.h"
+#include "fcs/operators/f_operator.h"
 
 namespace fcs {
 
-    static const double kF = 1.0;   // масштаб
+    // Линейный F-оператор
+    static const double kF = 1.0;
 
     double default_F(double delta) {
         return kF * delta;
     }
 
 }
-
